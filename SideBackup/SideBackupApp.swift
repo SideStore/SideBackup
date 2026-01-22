17e757d6a8fa3c983e99bae8fb6fc0bbb4bd7f9d
ny <mac@nythepegas.us>
2026-01-22 09:35:29 -0500

    Initial Commit

---
+++ SideBackup/SideBackupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideBackupApp.swift
+//  SideBackup
+//
+//  Created by ny on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct SideBackupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SideBackupTests/SideBackupTests.swift
@@ -0,0 +1,17 @@
+//
+//  SideBackupTests.swift
+//  SideBackupTests
+//
+//  Created by ny on 1/22/26.
+//
+
+import Testing
+@testable import SideBackup
+
+struct SideBackupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
